fe1d82202f040717dff2f2275f2a1b21baf14f10
hansen126 <hansen126187@gmail.com>
2024-09-26 03:21:11 +0700

    Update MBTI prediction

--- mbti/predict.py
+++ mbti/predict.py
@@ -1,18 +1,10 @@
-import pickle
-import numpy as np
-import re
 import pandas as pd
-import seaborn as sns
 import glob
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.preprocessing import FunctionTransformer
 from sklearn.svm import LinearSVC
 from sklearn.decomposition import TruncatedSVD
-from sklearn.metrics import accuracy_score, classification_report
-from sklearn.metrics import confusion_matrix
 
 # print test
 print("line test")
